3cac17e1d5e3b770a82d42ad081471748d249db7
Daniel Buompane <danielbuompane@gmail.com>
2016-03-01 18:31:04 -0500

    Pass 'can get repo name' test

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -4,11 +4,11 @@ class Repository
     @repo = HTTParty.get("https://api.github.com/users/#{username}/repos?access_token=#{ENV["GITHUB_TOKEN"]}")
   end
 
-  # def repo_name
-  #   names = []
-  #   @repo.each do |r|
-  #     names << r['name']
-  #   end
-  #   names
-  # end
+  def repo_name
+    names = []
+    @repo.each do |r|
+      names << r['name']
+    end
+    names
+  end
 end
