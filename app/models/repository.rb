781abc516f985dcb1a3d4119efccf671f70b3dba
Daniel Buompane <danielbuompane@gmail.com>
2016-03-01 15:36:41 -0500

    Make repository model test

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -3,4 +3,8 @@ class Repository
   def initialize(username)
     @repo = HTTParty.get("https://api.github.com/users/#{username}/repos", :headers => { 'Username' => 'dankb82', 'User-Agent' => 'dankb82'})
   end
+
+  def repo_name
+    
+  end
 end
